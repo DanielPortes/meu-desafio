516925dd51eec21d151602176a8be6e80cd5f756
portes <fagundes.daniel191@gmail.com>
2024-08-11 11:32:11 -0300

    nova view, ApiView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import ApiView from "@/views/ApiView.vue"
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: HomeView
+    },
+    {
+        path: '/api',
+        name: 'Api',
+        component: ApiView
     },
 ]
 
---
+++ src/views/ApiView.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-card class="mt-5">
+      <v-row justify="center">
+        <v-col cols="20" sm="16" md="20">
+          <v-card-title>Visualizacao da API</v-card-title>
+        </v-col>
+      </v-row>
+
+      <v-row justify="center">
+        <v-col cols="12">
+          <IBGEDataTable/>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import IBGEDataTable from '../components/IBGEDataTable.vue'
+
+export default {
+  name: 'About',
+  components: {
+    IBGEDataTable
+  }
+}
+</script>
