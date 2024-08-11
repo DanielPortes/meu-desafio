661907c56b11c06d34a6418507ad20d251a5ef96
portes <fagundes.daniel191@gmail.com>
2024-08-11 11:24:07 -0300

    criando rota para homeview

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import axios from 'axios'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+app.component('VueDatePicker', VueDatePicker)
+
+app.config.globalProperties.$axios = axios
+
+app.mount('#app')
