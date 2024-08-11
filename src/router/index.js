78f7654e634464bde7f9dab3644166ac3a04b7bc
portes <fagundes.daniel191@gmail.com>
2024-08-11 11:33:49 -0300

    nova view, NotFound.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ApiView from "@/views/ApiView.vue"
 import AboutView from '../views/AboutView.vue'
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
     {
@@ -19,6 +20,11 @@ const routes = [
         name: 'About',
         component: AboutView
     },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFound
+    }
 ]
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Página não encontrada</h1>
+    <p>A página que você está procurando não existe.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
